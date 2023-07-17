72c240faa3a61400f2c1dc7401f4315b3a6b68e9
niks <technoniks007@gmail.com>
2023-07-17 21:53:10 +0530

    using native fetch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { execSync } from 'child_process';
 import {resolve} from 'dns'
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 
 const url_base =  'https://art.victorytime.games'
 const url_login = '/api/user/login'
